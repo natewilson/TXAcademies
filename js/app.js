097b9c7871c72fd3cfb97894458d8c7b587cb36e
Nate Wilson <nate.wilson@gmail.com>
2013-01-01 01:05:53 -0500

    Working on layouts

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ function scroll(){
     theScroll = new iScroll('wrapper');
 }
 document.addEventListener('DOMContentLoaded', scroll, false);
+
+$(document).ready(function(){
+	$('#nav-bar a').on('click', function(e){
+	    e.preventDefault();
+	    var nextPage = $(e.target.hash);
+	    $("#pages .current").removeClass("current");
+	    nextPage.addClass("current");
+	});
+});
