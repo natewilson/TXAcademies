ae02eb46627f8283a3b3f45a5f212ab8a41365a6
Nate Wilson <nate.wilson@gmail.com>
2013-01-04 01:20:43 -0500

    I must not fully understand git b/c sometimes these aren't updating.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,20 @@ function scroll(){
 }
 //document.addEventListener('DOMContentLoaded', scroll, false);
 
+/**
+ * Function that fetches tweets and drops them into the #tweets 
+ * block using the tmpl-tweets template.
+ */
+function getTweets() {
+    var q = "txrsa"
+        rpp = 18,
+        twurl = "http://search.twitter.com/search.json?q=";
+    $.getJSON(twurl + q + "&rpp=" + rpp + "&callback=?", function(data){
+        $("#tmpl-tweets").tmpl(data.results).appendTo("#tweets");
+    });
+}
+
+/** Load this stuff up when the document loads **/
 $(document).ready(function(){
 	$('#nav-bar a').on('click', function(e){
 	    e.preventDefault();
@@ -11,4 +25,5 @@ $(document).ready(function(){
 	    $("#pages .current").removeClass("current");
 	    nextPage.addClass("current");
 	});
+	getTweets();
 });
