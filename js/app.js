dff2f99922d06bbfb38ca8f24ba9304f64f9571e
Nate Wilson <nate.wilson@gmail.com>
2013-01-31 00:51:47 -0500

    Testing the device-ready splash screen hiding.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
+/*
 var theScroll;
 function scroll(){
     theScroll = new iScroll('wrapper');
 }
+//*/
 //document.addEventListener('DOMContentLoaded', scroll, false);
+
+
+// Remove the Splash Screen
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	navigator.splashscreen.hide();
+}
 
 
 /** Load this stuff up when the document loads **/
 $(document).ready(function(){
+
+	// Add the click behaviour to the nav bar
 	$('#nav-bar a').on('click', function(e){
 	    e.preventDefault();
 	    var nextPage = $(this).attr("href");
 	    $("#pages .page").hide();
 	    $(nextPage).show();
 	});
+
+	// Show the home page
 	$("#pages .page").hide();
 	$("#page-home").show();
+
 });
 //*/
