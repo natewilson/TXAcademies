c1448654aa27f227fae5c64cab744b94b9a731c9
Nate Wilson <nate.wilson@gmail.com>
2013-01-31 00:18:45 -0500

    Added paging.

--- js/app.js
+++ js/app.js
@@ -4,26 +4,16 @@ function scroll(){
 }
 //document.addEventListener('DOMContentLoaded', scroll, false);
 
-/**
- * Function that fetches tweets and drops them into the #tweets 
- * block using the tmpl-tweets template.
- */
-function getTweets() {
-    var q = "txrsa"
-        rpp = 18,
-        twurl = "http://search.twitter.com/search.json?q=";
-    $.getJSON(twurl + q + "&rpp=" + rpp + "&callback=?", function(data){
-        $("#tmpl-tweets").tmpl(data.results).appendTo("#tweets");
-    });
-}
 
 /** Load this stuff up when the document loads **/
 $(document).ready(function(){
 	$('#nav-bar a').on('click', function(e){
 	    e.preventDefault();
-	    var nextPage = $(e.target.hash);
-	    $("#pages .current").removeClass("current");
-	    nextPage.addClass("current");
+	    var nextPage = $(this).attr("href");
+	    $("#pages .page").hide();
+	    $(nextPage).show();
 	});
-	getTweets();
+	$("#pages .page").hide();
+	$("#page-home").show();
 });
+//*/
